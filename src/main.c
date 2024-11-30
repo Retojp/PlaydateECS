4f15f117ba205e50d1937597038d3ea7c49b8d92
Piotr <piotr-sadkowski@o2.pl>
2024-11-30 18:57:53 +0100

    added repro to main

--- src/main.c
+++ src/main.c
@@ -1,6 +1,15 @@
 #include "src/Core/app.h"
+#include "src/Core/Utils/SparseSet/SparseSet.h"
 
 void SetupApplication(Application* app){
+    SparseSet* ss;
+    ss = SparseSetCreate(app->playdateApi->system->realloc,120,sizeof(int));
+    for (int i = 0; i < 120; i++)
+    {
+        app->playdateApi->system->logToConsole("%d",i);
+        SparseSetAdd(ss,i,&i);
+    }
+    
     return;
 }
 
